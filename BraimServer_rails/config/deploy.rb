569daf5745a5ed7cb6c1204667f59df7e2bdd7c5
Jesus Munoz <jesusduardo2028@gmail.com>
2015-03-23 21:06:07 -0500

    add configuration secet files of devise

--- BraimServer_rails/config/deploy.rb
+++ BraimServer_rails/config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:JesusEduardo2028/Braim.git'
 
 set :deploy_to, '/home/deploy/code/BraimServer_rails'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/mongoid.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do

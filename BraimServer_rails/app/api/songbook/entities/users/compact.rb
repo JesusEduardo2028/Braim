0360fa9b3f31a2f832efc6309fc3daf32935458f
Jesus Munoz <jesusduardo2028@gmail.com>
2015-03-27 17:10:07 -0500

    delete api user transformation

--- BraimServer_rails/app/api/songbook/entities/users/compact.rb
+++
@@ -1,9 +0,0 @@
-module Songbook
-  module Entities
-    module Users
-      class Compact < Grape::Entity
-        expose :email, documentation: { type: "email", desc: "user email", example: "allam_britto@example.com" }
-      end
-    end
-  end
-end

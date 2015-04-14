85540cf547c8aef9ad6594143609f7daaa5ba4fa
Jesus Munoz <jesusduardo2028@gmail.com>
2015-04-14 14:42:08 -0500

    add new ip staging server

--- BraimServer_rails/config/deploy/staging.rb
+++ BraimServer_rails/config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :stage, :staging
 
 # Replace 127.0.0.1 with your server's IP address!
-server '104.40.69.119', user: 'deploy', roles: %w{web app}
+server '104.40.72.171', user: 'deploy', roles: %w{web app}
